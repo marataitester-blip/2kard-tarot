d376926bcf6ca8d492916be57ac822242cbb9b33
marataitester-blip <marat.aitester@gmail.com>
2025-12-17 20:50:37 +0300

    Add TarotCard interface to types index

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface TarotCard {
+  id: string;
+  name: string;
+  imageUrl: string;
+  isVideo: boolean;
+  keyword: string;
+  desc_general: string;
+  desc_love: string;
+  desc_work: string;
+}
