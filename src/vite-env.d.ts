472c74f6a3bacee357de433023e45f6e00c4da2e
marataitester-blip <marat.aitester@gmail.com>
2025-12-23 17:08:42 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        // Эта строка позволяет использовать любые имена в папке api
-        rewrite: (path) => path.replace(/^\/api/, '') 
-      }
-    }
-  }
-});
+/// <reference types="vite/client" />
